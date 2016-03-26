dee3728c99a9ee76eb61eece1de319cbbc32b065
iossocket <avx302@gmail.com>
2016-03-26 16:14:42 +0800

    add mvc test method

---
+++ src/main/java/com/iossocket/controller/SpringMVCTest.java
@@ -0,0 +1,68 @@
+package com.iossocket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequestMapping("/springmvc")
+@Controller
+public class SpringMVCTest {
+
+
+    private static final String SUCCESS = "success";
+
+    // RequestMapping 可以修饰方法,也可修饰类
+    // 此时如果需要方法该类中的方法,使用  /springmvc/testRequestMapping
+    @RequestMapping("/testRequestMapping")
+    public String testRequestMapping() {
+        System.out.println("testRequestMapping");
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "/testMethod", method = RequestMethod.POST)
+    public String testMethod() {
+        System.out.println("testMehod");
+        return SUCCESS;
+    }
+
+    @RequestMapping(value = "testParamsAndHeader",
+            params = {"username", "age!=10"},
+            headers = {"username=zxl"}
+    )
+    public String testParamsAndHeader() {
+        System.out.println("testParamsAndHeader");
+        return SUCCESS;
+    }
+
+    @RequestMapping("/testAntPath/*/abc")
+    public String tsetAntPath() {
+        System.out.println("testAntPath");
+        return SUCCESS;
+    }
+
+
+    @RequestMapping("/testPathVariable/{id}")
+    public String testPathVariable(@PathVariable("id") Integer id) {
+        System.out.println("testPathVariable" + id);
+        return SUCCESS;
+    }
+
+    @RequestMapping("/testRequestParam")
+    public String testRequestParam(@RequestParam("username") String username,
+                                   @RequestParam(value = "password", required = false, defaultValue = "123") String password) {
+        System.out.println(username + "+" + password);
+        return SUCCESS;
+    }
+
+    @RequestMapping("/testRequestHeader")
+    public String testRequestHeader(@RequestHeader(value = "user", required = true, defaultValue = "123") String user) {
+        System.out.println("header user" + user);
+        return SUCCESS;
+    }
+
+    @RequestMapping("/testCookieValue")
+    public String testCookieValue(@CookieValue("JSESSIONID") String sessionId) {
+        System.out.println("Cookie value: " + sessionId);
+        return SUCCESS;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,5 +2,19 @@
 <body>
 <h2>Hello World!</h2>
 <a href="/hello">hello</a>
+
+<form action="/springmvc/testMethod" method="post">
+    <button type="submit">Test Post Method</button>
+</form>
+
+<br/>
+<a href="/springmvc/testParamsAndHeader?username=zxl&age=11">testParamsAndHeader</a>
+
+<br/>
+<a href="/springmvc/testAntPath/abac/abc">Test Ant Request</a>
+
+<br/>
+<a href="/springmvc/testPathVariable/12">testPathVariable</a>
 </body>
+
 </html>
